ae5629588e5bb7621406aeaf820388cc917bd79d
gloriousgu <gloriousgu@126.com>
2018-09-24 20:11:28 +0800

    add bounce from bottom

--- bounced-lib/src/main/java/com/gu/devel/bounced_lib/recyclerview/BouncedParent.java
+++ bounced-lib/src/main/java/com/gu/devel/bounced_lib/recyclerview/BouncedParent.java
@@ -1,22 +1,29 @@
 package com.gu.devel.bounced_lib.recyclerview;
 
+import android.animation.Animator;
 import android.animation.ValueAnimator;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.view.ViewCompat;
 import android.util.AttributeSet;
-import android.util.Log;
+import android.view.MotionEvent;
+import android.view.VelocityTracker;
 import android.view.View;
-import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.Interpolator;
+import android.view.animation.LinearInterpolator;
 import android.widget.FrameLayout;
-import android.widget.Scroller;
+import android.widget.OverScroller;
 
 public class BouncedParent extends FrameLayout {
   ObservableScroll mChild;
-  Scroller mScroller;
+  OverScroller mScroller;
   BounceAnimController mAnimController;
-  private static final int MAX_SPEED = 200;
+  VelocityTracker mVelocityTracker;
+  private static final int DO_PULL_ANIM_SPEED = 200;
+
+  private static final String TAG = BouncedParent.class.getSimpleName();
 
   public BouncedParent(@NonNull Context context) {
     this(context, null);
@@ -39,8 +46,8 @@ public class BouncedParent extends FrameLayout {
     postException("Not find Bounced Child,Please check your code !");
   }
 
-  public void startBounce() {
-    Log.e("TAG", "---------------------startBounce--------------------");
+  public void startBounce(boolean fromTop) {
+    mAnimController.config(fromTop ? mScroller.getCurrVelocity() : -mScroller.getCurrVelocity());
     ViewCompat.postOnAnimation(this, mAnimController);
   }
 
@@ -53,33 +60,66 @@ public class BouncedParent extends FrameLayout {
   }
 
   private void init() {
-    mScroller = new Scroller(getContext());
+    mVelocityTracker = VelocityTracker.obtain();
+    mScroller = new OverScroller(getContext(), sQuinticInterpolator);
     mAnimController = new BounceAnimController();
   }
 
-  public Scroller getScroller() {
+  public OverScroller getScroller() {
     return mScroller;
   }
 
   public boolean isOverSpeed() {
-    return true;
-    // Math.abs(mScroller.getCurrVelocity()) < MAX_SPEED;
+    return Math.abs(mScroller.getCurrVelocity()) > DO_PULL_ANIM_SPEED;
   }
 
-  class BounceAnimController implements Runnable, ValueAnimator.AnimatorUpdateListener {
+  @Override
+  public void computeScroll() {
+    super.computeScroll();
+    if (mScroller.computeScrollOffset()) {
+      postInvalidate();
+    }
+  }
+
+  @Override
+  public boolean dispatchTouchEvent(MotionEvent ev) {
+
+    return super.dispatchTouchEvent(ev);
+  }
+
+  // copy from RecyclerView
+  static final Interpolator sQuinticInterpolator =
+      new Interpolator() {
+        @Override
+        public float getInterpolation(float t) {
+          t -= 1.0f;
+          return t * t * t * t * t + 1.0f;
+        }
+      };
+
+  class BounceAnimController
+      implements Runnable, ValueAnimator.AnimatorUpdateListener, ValueAnimator.AnimatorListener {
 
     ValueAnimator anim;
+    private int bouncedDis;
+    boolean comeback;
+    DecelerateInterpolator downInterpolator;
+    LinearInterpolator upInterpolator;
 
     BounceAnimController() {
+      downInterpolator = new DecelerateInterpolator();
+      upInterpolator = new LinearInterpolator();
       anim = new ValueAnimator();
       anim.addUpdateListener(this);
-      anim.setInterpolator(new AccelerateDecelerateInterpolator());
-      config(-200);
+      anim.addListener(this);
     }
 
-    void config(int to) {
-      anim.setDuration(800);
-      anim.setIntValues(0, to, 0);
+    public void config(float velocity) {
+      comeback = false;
+      anim.setDuration(300);
+      bouncedDis = BouncedUtil.getDistanceByVelocity(velocity);
+      anim.setInterpolator(downInterpolator);
+      anim.setIntValues(0, -bouncedDis);
     }
 
     @Override
@@ -92,5 +132,24 @@ public class BouncedParent extends FrameLayout {
       int scrollY = (Integer) animation.getAnimatedValue();
       scrollTo(0, scrollY);
     }
+
+    @Override
+    public void onAnimationStart(Animator animation) {}
+
+    @Override
+    public void onAnimationEnd(Animator animation) {
+      if (!comeback) {
+        anim.setInterpolator(upInterpolator);
+        anim.setIntValues(-bouncedDis, 0);
+        anim.start();
+        comeback = true;
+      }
+    }
+
+    @Override
+    public void onAnimationCancel(Animator animation) {}
+
+    @Override
+    public void onAnimationRepeat(Animator animation) {}
   }
 }
---
+++ bounced-lib/src/main/java/com/gu/devel/bounced_lib/recyclerview/BouncedUtil.java
@@ -0,0 +1,22 @@
+package com.gu.devel.bounced_lib.recyclerview;
+
+public class BouncedUtil {
+  private static final int MIN_SPEED = 500;
+  private static final int MAX_SPEED = 5000;
+  private static final int MAX_OVER_DISTANCE = 300;
+  private static final int MIN_OVER_DISTANCE = 50;
+  private static final float rate =
+      (MAX_OVER_DISTANCE - MIN_OVER_DISTANCE) * 1.0f / (MAX_SPEED - MIN_SPEED);
+
+  // 通过速度计算惯性距离
+  public static int getDistanceByVelocity(float velocity) {
+    float signum = Math.signum(velocity);
+    return (int)
+        (signum * ((getAbsValidVelocity(velocity) - MIN_SPEED) * rate + MIN_OVER_DISTANCE));
+  }
+
+  private static float getAbsValidVelocity(float velocity) {
+    velocity = Math.abs(velocity);
+    return Math.min(Math.max(MIN_SPEED, velocity), MAX_SPEED);
+  }
+}
